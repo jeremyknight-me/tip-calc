48da5cfe8cd61554e2537e18c934b5728ca9fd60
Jeremy Knight <jeremy@theknights.me>
2021-05-01 15:47:51 -0500

    added simple view

---
+++ src/JK.TipCalc.Tests/Models/TipCalculationTests.cs
@@ -0,0 +1,27 @@
+﻿using JK.TipCalc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace JK.TipCalc.Tests.Models
+{
+    public class TipCalculationTests
+    {
+        [Theory]
+        [InlineData(10.0, 20, 2.0)]
+        [InlineData(100.0, 20, 20.0)]
+        public void Tip_Theories(decimal amount, int percent, decimal expected)
+        {
+            var sut = new TipCalculation
+            {
+                Amount = amount,
+                Percent = percent
+            };
+            var actual = sut.Tip;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ src/TipCalc/Models/TipCalculation.cs
@@ -0,0 +1,9 @@
+﻿namespace JK.TipCalc.Models
+{
+    public class TipCalculation
+    {
+        public decimal Amount { get; set; }
+        public int Percent { get; set; }
+        public decimal Tip => this.Amount * (this.Percent / 100m);
+    }
+}
