60a11008137321f865a6ce3891ede8dcd8899513
Jeremy <492328+jeremyknight-me@users.noreply.github.com>
2021-05-02 14:31:16 -0500

    initial dev (#4)
    * created project
    * removed unused files
    * removed tag to non-existant component
    * added SimpleView component
    * removed unused artifacts on main layout
    * renamed manifest.json to app.webmanifest
    * updated service worker to handle .webmanifest file types
    * added simple view

---
+++ src/TipCalc/Components/SimpleView.razor.cs
@@ -0,0 +1,25 @@
+﻿using JK.TipCalc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JK.TipCalc.Components
+{
+    public partial class SimpleView
+    {
+        protected IEnumerable<TipCalculation> PercentCalculations { get; private set; }
+
+        protected override void OnInitialized()
+        {
+            this.PercentCalculations = new List<TipCalculation>
+            {
+                new TipCalculation { Amount = 100m, Percent = 10 },
+                new TipCalculation { Amount = 100m, Percent = 15 },
+                new TipCalculation { Amount = 100m, Percent = 18 },
+                new TipCalculation { Amount = 100m, Percent = 20 },
+                new TipCalculation { Amount = 100m, Percent = 22 }
+            };
+        }
+    }
+}
