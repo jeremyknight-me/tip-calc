0b14f6a9faba47d7430b0ef7437c013773292494
Jeremy Knight <jeremy@theknights.me>
2021-05-30 15:59:43 -0500

    added total column

--- src/TipCalc/Models/TipCalculation.cs
+++ src/TipCalc/Models/TipCalculation.cs
@@ -5,5 +5,6 @@
         public decimal Amount { get; set; }
         public int Percent { get; set; }
         public decimal Tip => this.Amount * (this.Percent / 100m);
+        public decimal Total => this.Amount + this.Tip;
     }
 }
