b908b9d85f137f0f38ce7300a8a14b292d64b8d5
Jeremy <492328+jeremyknight-me@users.noreply.github.com>
2021-10-08 10:20:40 -0500

    added bootstrap v5 (#18)
    * updated header
    * updated tip list to use bootstrap

---
+++ src/TipCalc/Models/TipList.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace JK.TipCalc.Models
+{
+    public class TipList
+    {
+        private decimal amount;
+
+        public List<TipCalculation> TipCalculations { get; set; }
+        public TipCalculation CustomTip { get; set; }
+
+        public decimal Amount
+        {
+            get => this.amount;
+            set
+            {
+                this.amount = value;
+                this.CustomTip.Amount = value;
+                foreach (var calc in this.TipCalculations)
+                {
+                    calc.Amount = value;
+                }
+            }
+        }
+    }
+}
