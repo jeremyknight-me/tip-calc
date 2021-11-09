f1da72e6483b19c0622b6f0410a60c5e6129a3b0
Jeremy <492328+jeremyknight-me@users.noreply.github.com>
2021-11-09 18:10:37 -0500

    upgraded to dotnet6 (#23)

--- src/TipCalc/Models/TipList.cs
+++ src/TipCalc/Models/TipList.cs
@@ -1,25 +1,24 @@
 ﻿using System.Collections.Generic;
 
-namespace JK.TipCalc.Models
+namespace JK.TipCalc.Models;
+
+public class TipList
 {
-    public class TipList
+    private decimal amount;
+
+    public List<TipCalculation> TipCalculations { get; set; }
+    public TipCalculation CustomTip { get; set; }
+
+    public decimal Amount
     {
-        private decimal amount;
-
-        public List<TipCalculation> TipCalculations { get; set; }
-        public TipCalculation CustomTip { get; set; }
-
-        public decimal Amount
+        get => this.amount;
+        set
         {
-            get => this.amount;
-            set
+            this.amount = value;
+            this.CustomTip.Amount = value;
+            foreach (var calc in this.TipCalculations)
             {
-                this.amount = value;
-                this.CustomTip.Amount = value;
-                foreach (var calc in this.TipCalculations)
-                {
-                    calc.Amount = value;
-                }
+                calc.Amount = value;
             }
         }
     }
