bde782827387b73f40055ec3749a5fed2fa24948
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2022-12-10 11:18:15 -0600

    added round up functions and tests

--- src/JK.TipCalc.Common.Tests/Models/TipTests.cs
+++ src/JK.TipCalc.Common.Tests/Models/TipTests.cs
@@ -16,13 +16,25 @@ public class TipTests
     }
 
     [Theory]
-    [InlineData(0.80, 6.0, 5.20)]
-    [InlineData(0.790, 6.0, 5.21)]
+    [InlineData(0.3, 6.0, 5.70)]
+    [InlineData(2.79, 18.0, 15.21)]
     public void RoundDown_Theories(decimal expectedTip, decimal expectedTotal, decimal amount)
     {
         var sut = Tip.Create(20);
         sut.SetMealValues(amount, 0);
         sut.RoundDown();
+        Assert.Equal(expectedTip, sut.TipAmount);
+        Assert.Equal(expectedTotal, sut.Total);
+    }
+
+    [Theory]
+    [InlineData(1.3, 7.0, 5.70)]
+    [InlineData(3.79, 19.0, 15.21)]
+    public void RoundUp_Theories(decimal expectedTip, decimal expectedTotal, decimal amount)
+    {
+        var sut = Tip.Create(20);
+        sut.SetMealValues(amount, 0);
+        sut.RoundUp();
         Assert.Equal(expectedTip, sut.TipAmount);
         Assert.Equal(expectedTotal, sut.Total);
     }
