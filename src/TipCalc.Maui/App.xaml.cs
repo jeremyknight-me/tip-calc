cb555c2ea1b8d473669b7c73e0a4f05a36279e9d
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2024-03-16 23:23:32 -0500

    added maui app for dotnet8

---
+++ src/TipCalc.Maui/App.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace JK.TipCalc.Maui;
+
+public partial class App : Application
+{
+    public App()
+    {
+        this.InitializeComponent();
+        this.MainPage = new MainPage();
+    }
+}
