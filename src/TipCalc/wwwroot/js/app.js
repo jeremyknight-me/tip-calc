51af625f1056e0cf1df6f502e6c15748ab76f55d
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2025-06-04 23:28:30 -0500

    added guard against null prompt

--- src/TipCalc/wwwroot/js/app.js
+++ src/TipCalc/wwwroot/js/app.js
@@ -9,13 +9,17 @@ window.addEventListener('beforeinstallprompt', (e) => {
 
 window.tipcalc = {
     installApp: async function () {
-        // Show the install prompt
-        deferredInstallPrompt.prompt();
-        // Wait for the user to respond to the prompt
-        const { outcome } = await deferredInstallPrompt.userChoice;
-        // Optionally, send analytics event with outcome of user choice
-        console.log(`User response to the install prompt: ${outcome}`);
-        // We've used the prompt and can't use it again, throw it away
-        deferredInstallPrompt = null;
+        if (deferredInstallPrompt) {
+            // Show the install prompt
+            deferredInstallPrompt.prompt();
+            // Wait for the user to respond to the prompt
+            const { outcome } = await deferredInstallPrompt.userChoice;
+            // Optionally, send analytics event with outcome of user choice
+            console.log(`User response to the install prompt: ${outcome}`);
+            // We've used the prompt and can't use it again, throw it away
+            deferredInstallPrompt = null;
+        } else {
+            console.warn('Install prompt is not available.');
+        }
     }
 }
